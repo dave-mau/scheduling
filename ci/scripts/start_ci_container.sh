e6bdf0f3d836565055565d7a69fbc48dd390cd46
David Mauderli <davidmauderli@gmail.com>
2024-07-02 01:04:46 +0200

    improve returncode

--- ci/scripts/start_ci_container.sh
+++ ci/scripts/start_ci_container.sh
@@ -2,3 +2,4 @@
 docker stop ci-container > /dev/null 2>&1
 # Create the ci-container from the scheduling image.
 docker run --rm -dit --name ci-container --mount type=bind,source="$(pwd)",target=/home/ci/workspace davemau/scheduling
+exit $?
