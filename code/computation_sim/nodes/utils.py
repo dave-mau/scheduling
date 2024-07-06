f1853a4fd58d54c848fcabd2e58ce9f987f93b3d
David Mauderli <davidmauderli@gmail.com>
2024-07-06 13:16:41 +0200

    fix typing of header_to_state

--- code/computation_sim/nodes/utils.py
+++ code/computation_sim/nodes/utils.py
@@ -3,7 +3,7 @@ from computation_sim.time import as_age
 from typing import Tuple
 
 
-def header_to_state(header: Header, now: Time) -> Tuple[float, float, float, float]:
+def header_to_state(header: Header, now: Time) -> Tuple[float, float, float]:
     return [
         float(as_age(header.t_measure_youngest, now)),
         float(as_age(header.t_measure_average, now)),
