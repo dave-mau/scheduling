004e3a95cfc5655e8b0caf36a46c1c05b6740da9
David Mauderli <davidmauderli@gmail.com>
2024-07-08 21:44:22 +0200

    init order

--- code/computation_sim/nodes/__init__.py
+++ code/computation_sim/nodes/__init__.py
@@ -1,6 +1,6 @@
-from .ring_buffer_node import RingBufferNode
 from .node import Node, Visitor
 from .output_node import OutputNode
+from .ring_buffer_node import RingBufferNode
 from .sink_node import SinkNode
 from .source_node import Sensor, PeriodicEpochSensor, SourceNode
 from .utils import header_to_state
