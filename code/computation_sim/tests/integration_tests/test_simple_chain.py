2c93a01d720da669e717168ee946fe84de187c51
David Mauderli <davidmauderli@gmail.com>
2024-07-20 22:06:40 +0200

    Revert "moved system fixtures to separate test module"
    This reverts commit bd5629d46811a9328a6322c6e2e05d8095bb5507.

--- code/computation_sim/tests/integration_tests/test_simple_chain.py
+++ code/computation_sim/tests/integration_tests/test_simple_chain.py
@@ -1,10 +1,53 @@
+from typing import Tuple
+
 import numpy as np
+import pytest
+from computation_sim.nodes import (
+    FilteringMISONode,
+    Node,
+    OutputNode,
+    PeriodicEpochSensor,
+    SinkNode,
+    SourceNode,
+)
+from computation_sim.system import Action, System
+from computation_sim.time import Clock, FixedDuration, TimeProvider
 
-from ..system_fixtures import simple_chain
+
+@pytest.fixture
+def setup() -> Tuple[System, Clock, Tuple[Node]]:
+    clock = Clock(0)
+
+    # Setup the source
+    sensor = PeriodicEpochSensor(0, 100, FixedDuration(0))
+    source_node = SourceNode(clock.as_readonly(), sensor, "SOURCE_NODE")
+    # Setup the processor node
+    compute_node = FilteringMISONode(clock.as_readonly(), FixedDuration(10), id="COMPUTE_NODE")
+    # Setup the output node
+    output_node = OutputNode(clock.as_readonly(), id="OUTPUT_NODE")
+    # Setup a sink for failing compute
+    sink_node = SinkNode(clock.as_readonly(), id="SINK_NODE")
+
+    # Connect outputs
+    source_node.add_output(compute_node)
+    compute_node.set_output_pass(output_node)
+    compute_node.set_output_fail(sink_node)
+
+    # Set-up the action
+    compute_action = Action(name="ACT_COMPUTE_NODE")
+    compute_action.register_callback(compute_node.trigger, 0)
+
+    system = System()
+    system.add_action(compute_action)
+    system.add_node(sink_node)
+    system.add_node(output_node)
+    system.add_node(compute_node)
+    system.add_node(source_node)
+    return system, clock, (source_node, compute_node, output_node, sink_node)
 
 
-def test_no_action_no_output(simple_chain):
-    system, clock, nodes = simple_chain
+def test_no_action_no_output(setup):
+    system, clock, nodes = setup
     while clock.get_time() < 1000:
         clock += 1
         system.update()
@@ -12,8 +55,8 @@ def test_no_action_no_output(simple_chain):
     assert nodes[2].last_received is None
 
 
-def test_nominal(simple_chain):
-    system, clock, nodes = simple_chain
+def test_nominal(setup):
+    system, clock, nodes = setup
     # state vector:
     # [compute_busy, clock-time, output has result, ..output-times.., sink-count]
 
