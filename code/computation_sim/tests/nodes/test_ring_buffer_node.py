26d42e402fd8b171cf05aa83d2ceccb5e07cdf20
David Mauderli <davidmauderli@gmail.com>
2024-07-21 10:27:10 +0200

    ring buffer node: added separate overflow output instead of cb

--- code/computation_sim/tests/nodes/test_ring_buffer_node.py
+++ code/computation_sim/tests/nodes/test_ring_buffer_node.py
@@ -1,19 +1,19 @@
 from unittest.mock import MagicMock, Mock
 
 import pytest
-from computation_sim.basic_types import Header, Message
+from computation_sim.basic_types import BadNodeGraphError, Header, Message
 from computation_sim.nodes import ConstantNormalizer, RingBufferNode
 
 
 @pytest.fixture
-def setup_no_callback():
+def setup_no_outputs():
     clock_mock = Mock()
     buffer = RingBufferNode(clock_mock, max_num_elements=2)
     return buffer, clock_mock
 
 
-def test_receive_no_overflow_0(setup_no_callback):
-    buffer, _ = setup_no_callback
+def test_receive_no_overflow_0(setup_no_outputs):
+    buffer, _ = setup_no_outputs
 
     assert buffer.num_entries == 0
 
@@ -33,8 +33,8 @@ def test_receive_no_overflow_0(setup_no_callback):
     assert result is None
 
 
-def test_receive_overflow_0(setup_no_callback):
-    buffer, _ = setup_no_callback
+def test_receive_overflow_0(setup_no_outputs):
+    buffer, _ = setup_no_outputs
 
     assert buffer.num_entries == 0
 
@@ -57,62 +57,64 @@ def test_receive_overflow_0(setup_no_callback):
     assert result is None
 
 
+def test_trigger_raises(setup_no_outputs):
+    buffer, _ = setup_no_outputs
+    with pytest.raises(BadNodeGraphError):
+        buffer.trigger()
+
+
 @pytest.fixture
-def setup_callback():
-    clock_mock = Mock()
-    cb_mock = MagicMock()
-    buffer = RingBufferNode(clock_mock, max_num_elements=2, overflow_cb=cb_mock)
-    return buffer, clock_mock, cb_mock
+def setup_outputs():
+    clock_mock = MagicMock()
+    output_mock = MagicMock()
+    overflow_mock = MagicMock()
+    buffer = RingBufferNode(clock_mock, max_num_elements=2)
+    buffer.set_output(output_mock)
+    buffer.set_overflow_output(overflow_mock)
+    return buffer, clock_mock, output_mock, overflow_mock
 
 
-def test_receive_no_overflow_1(setup_callback):
-    buffer, _, cb_mock = setup_callback
+def test_receive_no_overflow_1(setup_outputs):
+    buffer, _, _, overflow_mock = setup_outputs
 
     buffer.receive(Message(Header(1, 2, 3)))
     buffer.receive(Message(Header(4, 5, 6)))
-    assert not cb_mock.called
+    assert not overflow_mock.receive.called
 
 
-def test_receive_overflow_1(setup_callback):
-    buffer, _, cb_mock = setup_callback
+def test_receive_overflow_1(setup_outputs):
+    buffer, _, _, overflow_mock = setup_outputs
 
     buffer.receive(Message(Header(1, 2, 3)))
     buffer.receive(Message(Header(4, 5, 6)))
     buffer.receive(Message(Header(5, 6, 7)))
-    assert cb_mock.call_count == 1
-    assert cb_mock.call_args[0][0].header.t_measure_oldest == 1
+    assert overflow_mock.receive.call_count == 1
+    assert overflow_mock.receive.call_args[0][0].header.t_measure_oldest == 1
     buffer.receive(Message(Header(8, 9, 10)))
-    assert cb_mock.call_count == 2
-    assert cb_mock.call_args[0][0].header.t_measure_oldest == 4
+    assert overflow_mock.receive.call_count == 2
+    assert overflow_mock.receive.call_args[0][0].header.t_measure_oldest == 4
 
 
-def test_trigger_empty(setup_no_callback):
-    buffer, _ = setup_no_callback
-    output_mock = Mock()
-    buffer.add_output(output_mock)
+def test_trigger_empty(setup_outputs):
+    buffer, _, output_mock, _ = setup_outputs
 
     buffer.trigger()
-
     assert output_mock.receive.call_count == 0
 
 
-def test_trigger_full(setup_no_callback):
-    buffer, _ = setup_no_callback
-    output_mock = Mock()
-    buffer.add_output(output_mock)
-
+def test_trigger_full(setup_outputs):
+    buffer, _, output_mock, _ = setup_outputs
     buffer.receive(Message(Header(1, 1, 1)))
     buffer.receive(Message(Header(2, 2, 2)))
     buffer.receive(Message(Header(3, 3, 3)))
-
     buffer.trigger()
 
     assert output_mock.receive.call_count == 1
     assert output_mock.receive.call_args[0][0].header.t_measure_oldest == 2
 
 
-def test_pop(setup_no_callback):
-    buffer, _ = setup_no_callback
+def test_pop(setup_no_outputs):
+    buffer, _ = setup_no_outputs
     buffer.receive(Message(Header(1, 1, 1)))
     buffer.receive(Message(Header(2, 2, 2)))
 
@@ -128,8 +130,8 @@ def test_pop(setup_no_callback):
     assert result is None
 
 
-def test_reset(setup_no_callback):
-    buffer, _ = setup_no_callback
+def test_reset(setup_no_outputs):
+    buffer, _ = setup_no_outputs
 
     buffer.receive(Message(Header(1, 1, 1)))
     assert buffer.num_entries == 1
@@ -137,8 +139,8 @@ def test_reset(setup_no_callback):
     assert buffer.num_entries == 0
 
 
-def test_state_full_default_normalizer(setup_no_callback):
-    buffer, clock_mock = setup_no_callback
+def test_state_full_default_normalizer(setup_no_outputs):
+    buffer, clock_mock = setup_no_outputs
     clock_mock.time = 6
     buffer.receive(Message(Header(1, 2, 3)))
     buffer.receive(Message(Header(4, 5, 6)))
@@ -156,8 +158,8 @@ def test_state_full_default_normalizer(setup_no_callback):
         next(result)
 
 
-def test_state_empty_default_normalizer(setup_no_callback):
-    buffer, clock_mock = setup_no_callback
+def test_state_empty_default_normalizer(setup_no_outputs):
+    buffer, clock_mock = setup_no_outputs
     clock_mock.time = 6
 
     result = buffer.generate_state()
@@ -173,8 +175,8 @@ def test_state_empty_default_normalizer(setup_no_callback):
         next(result)
 
 
-def test_state_semifull_default_normalizer(setup_no_callback):
-    buffer, clock_mock = setup_no_callback
+def test_state_semifull_default_normalizer(setup_no_outputs):
+    buffer, clock_mock = setup_no_outputs
     clock_mock.time = 6
     buffer.receive(Message(Header(1, 2, 3)))
 
