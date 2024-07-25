3231e5ef750d5dba2a71ccdf249fba250cc543a3
David Mauderli <davidmauderli@gmail.com>
2024-07-25 18:19:22 +0200

    added utils for action unpacking

--- code/computation_sim/system/__init__.py
+++ code/computation_sim/system/__init__.py
@@ -1,4 +1,4 @@
-from .action import Action
+from .action import Action, unpack_action, max_action_id, num_actions 
 from .builder import SystemBuidler
 from .system import System
 from .system_drawer import SystemDrawer
--- code/computation_sim/system/action.py
+++ code/computation_sim/system/action.py
@@ -1,3 +1,5 @@
+import numpy as np
+from math import log2, floor
 from collections import namedtuple
 from typing import Callable
 
@@ -31,3 +33,37 @@ class Action(object):
 
     def clear(self) -> None:
         self._callbacks.clear()
+
+def max_action_id(num_action_dims: int) -> int:
+    """ Returns the largest common action id, given a number of individual actions.
+
+    For example, given a 3D action vector , the largest action vector is ([1, 1, 1]), which is 7 in decimal.
+    """
+    return 2**num_action_dims - 1
+
+def num_actions(num_action_dims: int) -> int:
+    """ Returns the number of common actions, given a number of individual actions.
+
+    For example, given a three individual actions, there are 8 possible combinations.
+    """
+    if num_action_dims > 0:
+        return max_action_id(num_action_dims) + 1
+    else:
+        return 0
+
+def unpack_action(num_action_dims: int, action: int) -> np.ndarray:
+    """ Given a a packed action `action`, convert it into its binary form.
+
+    Example:
+        ```
+        assert unpack_action(3, 1) == [0, 0, 1]
+        assert unpack_action(3, 2) == [0, 1, 0]
+        assert unpack_action(3, 7) == [1, 1, 1]
+    """
+    num_actions = max_action_id(num_action_dims)
+    assert action >= 0 and action <= num_actions,f"Invalid action id {action}."
+    assert 0 < num_actions <= 255, f"Maximum number of actions is 255."
+    bits = np.unpackbits(np.uint8(action))
+    return bits[-num_action_dims:]
+
+
