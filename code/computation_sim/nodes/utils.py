52bd13f253d47d8ffde687678632053e45e542ec
David Mauderli <davidmauderli@gmail.com>
2024-09-17 22:37:11 +0200

    header to state dumps counts as well

--- code/computation_sim/nodes/utils.py
+++ code/computation_sim/nodes/utils.py
@@ -3,10 +3,31 @@ from typing import Tuple
 from computation_sim.basic_types import Header, Time
 from computation_sim.time import as_age
 
+from .interfaces import StateVariableNormalizer
+from .state_normalizers import ConstantNormalizer
 
-def header_to_state(header: Header, now: Time) -> Tuple[float, float, float]:
+
+def header_to_state(
+    header: Header,
+    now: Time,
+    age_normalizer: StateVariableNormalizer = ConstantNormalizer(1.0),
+    count_normalizer: StateVariableNormalizer = ConstantNormalizer(1.0),
+) -> Tuple[float, float, float, float]:
     return [
-        float(as_age(header.t_measure_oldest, now)),
-        float(as_age(header.t_measure_youngest, now)),
-        float(as_age(header.t_measure_average, now)),
+        age_normalizer.normalize(float(as_age(header.t_measure_oldest, now))),
+        age_normalizer.normalize(float(as_age(header.t_measure_youngest, now))),
+        age_normalizer.normalize(float(as_age(header.t_measure_average, now))),
+        count_normalizer.normalize(float(header.num_measurements)),
+    ]
+
+
+def empty_message_state(
+    age_normalizer: StateVariableNormalizer = ConstantNormalizer(1.0),
+    count_normalizer: StateVariableNormalizer = ConstantNormalizer(1.0),
+) -> Tuple[float, float, float, float]:
+    return [
+        age_normalizer.normalize(0.0),
+        age_normalizer.normalize(0.0),
+        age_normalizer.normalize(0.0),
+        count_normalizer.normalize(0.0),
     ]
--- code/computation_sim/tests/nodes/test_utils.py
+++ code/computation_sim/tests/nodes/test_utils.py
@@ -1,10 +1,20 @@
 import pytest
 from computation_sim.basic_types import Header
-from computation_sim.nodes import header_to_state
+from computation_sim.nodes import header_to_state, empty_message_state
 
 
 def test_header_to_state_pass():
-    result = header_to_state(Header(1, 2, 3), 4)
+    result = header_to_state(Header(1, 2, 3, 11), 4)
+    assert len(result) == 4
     result[0] == pytest.approx(3.0, 1.0e-6)
     result[1] == pytest.approx(2.0, 1.0e-6)
     result[2] == pytest.approx(1.0, 1.0e-6)
+    result[3] == pytest.approx(11.0, 1.0e-6)
+
+def test_empty_message_state_pass():
+    result = empty_message_state()
+    assert len(result) == 4
+    result[0] == pytest.approx(0.0, 1.0e-6)
+    result[1] == pytest.approx(0.0, 1.0e-6)
+    result[2] == pytest.approx(0.0, 1.0e-6)
+    result[3] == pytest.approx(0.0, 1.0e-6)
