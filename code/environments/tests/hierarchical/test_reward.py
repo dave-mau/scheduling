9efc08658fd2cd98fc7efb532a671f19ce8f2dd6
David Mauderli <davidmauderli@gmail.com>
2024-12-08 13:23:25 +0100

    update reward

--- code/environments/tests/hierarchical/test_reward.py
+++ code/environments/tests/hierarchical/test_reward.py
@@ -1,7 +1,22 @@
-from environments.hierarchical import Reward
 import pytest
+from environments.hierarchical import Reward
+from unittest.mock import Mock
 
 
-def test_dummy():
-    assert True
+@pytest.fixture
+def reward():
+    return Reward(cost_message_loss=1.0, cost_output_age=10.0, cost_activation=100.0)
 
+def test_action_reward_happy(reward):
+    action = [1, 2, 3]
+    buffer_overrides = {'a': 1, 'b': 2}
+    missing_inputs = {'c': 3, 'd': 4}
+    output_age = 5
+    assert pytest.approx(reward(action, buffer_overrides, missing_inputs, output_age)) == - (1.0 * 10 + 10.0 * 5 + 100.0 * 3)
+
+def test_action_reward_empty(reward):
+    action = []
+    buffer_overrides = {}
+    missing_inputs = {}
+    output_age = 5
+    assert pytest.approx(reward(action, buffer_overrides, missing_inputs, output_age)) == - (5.0 * 10.0)
