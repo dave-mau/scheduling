d745fd83e866162afe540b44eaa7bfc96b08614d
David Mauderli <davidmauderli@gmail.com>
2024-12-08 14:55:31 +0100

    fixed seeds

---
+++ code/computation_sim/tests/time/test_duration_samplers.py
@@ -0,0 +1,30 @@
+from computation_sim.time.duration_samplers import FixedDuration, GaussianTimeSampler, GammaDistributionSampler
+import pytest
+
+
+def test_fixed_duration():
+    sampler = FixedDuration(10)
+    assert sampler.sample() == 10
+    assert sampler.sample() == 10
+    assert sampler.sample() == 10
+    assert sampler.sample() == 10
+
+def test_gaussian():
+    sampler = GaussianTimeSampler(0.0, 1.0, 1.0, 1.0)
+    sampler.reset(0)
+    data_0 = [sampler.sample() for _ in range(10)]
+
+    sampler.reset(0)
+    data_1 = [sampler.sample() for _ in range(10)]
+
+    assert data_0 == data_1
+
+def test_gamma():
+    sampler = GammaDistributionSampler(1.0, 2.0)
+    sampler.reset(0)
+    data_0 = [sampler.sample() for _ in range(10)]
+
+    sampler.reset(0)
+    data_1 = [sampler.sample() for _ in range(10)]
+
+    assert data_0 == data_1
--- code/computation_sim/time/duration_samplers.py
+++ code/computation_sim/time/duration_samplers.py
@@ -7,13 +7,24 @@ from .clock import round_to_fixed_point
 
 
 class DurationSampler(ABC):
+    def __init__(self, seed: int = None):
+        self._rng = np.random.default_rng(seed)
+
+    @property
+    def rng(self) -> np.random.Generator:
+        return self
+
+    def reset(self, seed: int = None):
+        self._rng = np.random.default_rng(seed)
+
     @abstractmethod
     def sample(self) -> Time:
         pass
 
 
 class FixedDuration(DurationSampler):
-    def __init__(self, val: Time):
+    def __init__(self, val: Time, **kwargs):
+        super().__init__(**kwargs)
         self.val = val
 
     @round_to_fixed_point
@@ -22,7 +33,8 @@ class FixedDuration(DurationSampler):
 
 
 class GaussianTimeSampler(DurationSampler):
-    def __init__(self, mu: float, std: float, gain: float, offset=float):
+    def __init__(self, mu: float, std: float, gain: float, offset: float, **kwargs):
+        super().__init__(**kwargs)
         self._mu = mu
         self._std = std
         self._gain = gain
@@ -32,12 +44,13 @@ class GaussianTimeSampler(DurationSampler):
     def sample(self) -> Time:
         res = -1
         while res < 0:
-            res = np.random.normal(self._mu, self._std) * self._gain + self._offset
+            res = self._rng.normal(self._mu, self._std) * self._gain + self._offset
         return res
 
 
 class GammaDistributionSampler(DurationSampler):
-    def __init__(self, k, theta, gain: float = 1.0, offset: float = 0.0):
+    def __init__(self, k, theta, gain: float = 1.0, offset: float = 0.0, **kwargs):
+        super().__init__(**kwargs)
         self._k = k
         self._theta = theta
         self._gain = gain
@@ -45,4 +58,4 @@ class GammaDistributionSampler(DurationSampler):
 
     @round_to_fixed_point
     def sample(self) -> Time:
-        return np.random.gamma(self._k, self._theta) * self._gain + self._offset
+        return self._rng.gamma(self._k, self._theta) * self._gain + self._offset
