c4a3b3b44e580be2c151cd445fe56bca33013d57
David Mauderli <davidmauderli@gmail.com>
2024-12-08 13:24:07 +0100

    format init

--- code/environments/hierarchical/__init__.py
+++ code/environments/hierarchical/__init__.py
@@ -1,4 +1,4 @@
-from .reward import Reward
-from .types import ActionCollection, SystemCollection
 from .builder import HierarchicalSystemBuilder
 from .env_base import HierarchicalSystemBase
+from .reward import Reward
+from .types import ActionCollection, SystemCollection
