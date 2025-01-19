3c6869e40bdb9bd2b3082ea9d95c4836388b3949
David Mauderli <davidmauderli@gmail.com>
2025-01-19 19:04:52 +0100

    improve format

--- format_all.sh
+++ format_all.sh
@@ -1,3 +1,4 @@
 #! /usr/bin/bash
-black . --workers 8 --quiet -l 119 --exclude 'rl-baselines3-zoo'
-isort --profile black --skip 'rl-baselines3-zoo' . 
+EXCLUDE_DIRS="rl-baselines3-zoo"
+black . --workers 8 --quiet -l 119 --exclude $EXCLUDE_DIRS
+isort --profile black --skip $EXCLUDE_DIRS . 
