c3471e05f441655cf0302b6feca47d9ff21c09f2
David Mauderli <davidmauderli@gmail.com>
2025-01-19 19:04:19 +0100

    format ignore rl-baseline-zoo

--- format_all.sh
+++ format_all.sh
@@ -1,3 +1,3 @@
 #! /usr/bin/bash
-black . --workers 8 --quiet -l 119
-isort --profile black .
+black . --workers 8 --quiet -l 119 --exclude 'rl-baselines3-zoo'
+isort --profile black --skip 'rl-baselines3-zoo' . 
